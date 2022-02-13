12a5ff491faa16134cd6df7d7f9b7918e57db3ab
blackphoenix <blackphoenix@pop-os.localdomain>
2022-02-13 22:59:22 +0545

    error resolved

--- class and objects/classObject.py
+++ class and objects/classObject.py
@@ -4,56 +4,51 @@ class Car:
     def __init__(self, top_speed):
         self.top_speed = top_speed
         self.speed = 0
-        self.const_speed = 10
     
     def start(self):
         print("Starting your car...")
-
+    
     def move(self):
         self.speed += 2
-        print("\nCar is moving at a speed of "+str(self.speed)+"kmph although it's max speed is: "+str(self.top_speed))
+        print("\nCar is moving at speed of "+str(self.speed)+" kmph although it's max speed is "+str(self.top_speed))
         print("\n moving...")
-        return self.const_speed
-
-    def accelerate(self):
-        self.speed += 2
-        print(("\n moving..."))
         return self.speed
-
+    
+    def acclerate(self):
+        self.speed += 2
+        print("\n moving...")
+        return self.speed
+    
     def apply_brake(self):
         self.speed = 0
-    
 
 def callCar():
-    print("Note: Press 'b' to hit brake")
-    top_speed = int(input("\nEnter top speed in km/h: "))
-    bmw = Car(top_speed)      
+    print("Note: Press 'b' to hit the brake")
+    top_speed = int(input("Enter the top spped in km/h: "))
+    avg_speed = int(input("Enter your average speed: "))
+    bmw = Car(top_speed)
     bmw.start()
-    
+
     while True:
-        const_speed = bmw.move()
+        speed = bmw.move()
         time.sleep(3)
+
+        choice = input("\n Do you want to accelerate? y/n: ")
+        if choice == 'Y' or choice=='y':
+            speed = bmw.acclerate()
+            print("\n Accelerating at a speed of "+str(speed))
         
-        # option to accelerate
-        choice = input("\nDo you want to accelerate? Y/N: ")
-        if choice == "Y" or choice=="y":
-            speed = bmw.accelerate()
-            print("\nAccelerating at a speed of "+str(speed))
+        if speed > avg_speed:
+            print("\n ******Warning!! You're speeding up******")
         
-        # giving a warning if the speed exceeds constant speed
-        if speed > const_speed:
-            print("\n*****Warning! You're speeding up*****")
-        
-        # applying brake if the speed tries to exceed top speed 
         if speed > top_speed:
-            print("You have exceeded the top speed. Press 'b' to hit brake")
+            print("You have exceeded the top speed. Press 'b' to hit the brake")
             inp = input()
-            if inp == "b":
-                print("Applying brake. Car is stopping...")
+            if inp == 'b':
+                print("Applying brake. The car is stopping...")
                 time.sleep(2)
                 break
         else:
             continue
 
 callCar()
-
